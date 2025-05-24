e829bc1a9d2de9abd33df88a23f1a95de698cf18
yxm <yxm.dev@gmx.ie>
2025-05-24 13:43:29 -0300

    feat: improvements in 'path' lib

--- utils/mods/path.py
+++ utils/mods/path.py
@@ -1,19 +1,49 @@
+from typing import Tuple, Union
 import os
 
 class path:
-    def join(*args):
-        os.path.join(*args)
+    def exists(*paths: Tuple[str]) -> bool:
+        return all(os.path.exists(path_) for path_ in paths)
 
-    def basename(path_):
-        return os.path.basename(path_)
+    def is_file(*paths: Tuple[str]) -> bool:
+        return all(os.path.isfile(path_) for path_ in paths)
 
-    def dirname(path_):
-        return os.path.dirname(path_)
+    def is_dir(*paths: Tuple[str]) -> bool:
+        return all(os.path.isdir(path_) for path_ in paths)
 
-    def filename(path_):
-        y = path.basename(path_)
-        return y.split('.')[0]
+    def is_abs(*paths: Tuple[str]) -> bool:
+        return all(os.path.isabs(path_) for path_ in paths)
 
-    def ext(path_):
-        y = path.basename(path_)
-        return y.split('.')[1]
+    def is_rel(*paths: Tuple[str]) -> bool:
+        return not any(os.path.isabs(path_) for path_ in paths)
+
+    def is_link(*paths: Tuple[str]) -> bool:
+        return not any(os.path.islink(path_) for path_ in paths)
+
+    def join(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+        return os.path.join(*paths)
+
+    def basename(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+        if len(paths) == 1:
+            return os.path.basename(paths[0])
+        else:
+            return (os.path.basename(path_) for path_ in paths)
+
+    def dirname(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+        if len(paths) == 1:
+            return os.path.dirname(paths[0])
+        else:
+            return (os.path.dirname(path_) for path_ in paths)
+
+    def filename(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+        if len(paths) == 1:
+            return path.basename(paths[0]).split('.')[0]
+        else:
+            return (path.basename(path_).split('.')[0] for path_ in paths)
+
+    def extension(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+        if len(paths) == 1:
+            return path.basename(paths[0]).split('.')[1]
+        else:
+            return (path.basename(path_).split('.')[1] for path_ in paths)
+
