a091fcd59a20a94b53f5c71ca9a2bb6673ca400f
yxm <yxm.dev@gmx.ie>
2025-05-26 10:02:01 -0300

    ref: include checking for Path in 'path' lib

--- utils/mods/path.py
+++ utils/mods/path.py
@@ -1,49 +1,71 @@
-from typing import Tuple, Union
 import os
+from utils.mods.types import Path, Tuple, Union
+from utils.err import PathErr
 
 class path:
-    def exists(*paths: Tuple[str]) -> bool:
-        return all(os.path.exists(path_) for path_ in paths)
+    def exists(*paths: Tuple[Path]) -> bool:
+        try:
+            return all(os.path.exists(Path(path_)) for path_ in paths)
+        except Exception as e:
+            raise PathErr(e)
 
-    def is_file(*paths: Tuple[str]) -> bool:
-        return all(os.path.isfile(path_) for path_ in paths)
+    def is_file(*paths: Tuple[Path]) -> bool:
+        try:
+            return all(os.path.isfile(Path(path_)) for path_ in paths)
+        except Exception as e:
+            raise PathErr(e)
 
-    def is_dir(*paths: Tuple[str]) -> bool:
-        return all(os.path.isdir(path_) for path_ in paths)
+    def is_dir(*paths: Tuple[Path]) -> bool:
+        try:
+            return all(os.path.isdir(Path(path_)) for path_ in paths)
+        except Exception as e:
+            raise PathErr(e)
 
-    def is_abs(*paths: Tuple[str]) -> bool:
-        return all(os.path.isabs(path_) for path_ in paths)
+    def is_abs(*paths: Tuple[Path]) -> bool:
+        try:
+            return all(os.path.isabs(Path(path_)) for path_ in paths)
+        except Exception as e:
+            raise PathErr(e)
 
-    def is_rel(*paths: Tuple[str]) -> bool:
-        return not any(os.path.isabs(path_) for path_ in paths)
+    def is_rel(*paths: Tuple[Path]) -> bool:
+        try:
+            return not any(os.path.isabs(Path(path_)) for path_ in paths)
+        except Exception as e:
+            raise PathErr(e)
 
-    def is_link(*paths: Tuple[str]) -> bool:
-        return not any(os.path.islink(path_) for path_ in paths)
+    def is_link(*paths: Tuple[Path]) -> bool:
+        try:
+            return not any(os.path.islink(Path(path_)) for path_ in paths)
+        except Exception as e:
+            raise PathErr(e)
 
-    def join(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
-        return os.path.join(*paths)
+    def join(*paths: Tuple[Path]) -> Union[Path, Tuple[Path]]:
+        try:
+            return os.path.join(*(Path(path_) for path_ in paths))
+        except Exception as e:
+            raise PathErr(e)
 
-    def basename(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+    def basename(*paths: Tuple[Path]) -> Union[str, Tuple[str]]:
         if len(paths) == 1:
-            return os.path.basename(paths[0])
+            return os.path.basename(Path(paths[0]))
         else:
-            return (os.path.basename(path_) for path_ in paths)
+            return (os.path.basename(Path(path_)) for path_ in paths)
 
-    def dirname(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+    def dirname(*paths: Tuple[Path]) -> Union[str, Tuple[str]]:
         if len(paths) == 1:
-            return os.path.dirname(paths[0])
+            return os.path.dirname(Path(paths[0]))
         else:
-            return (os.path.dirname(path_) for path_ in paths)
+            return (os.path.dirname(Path(path_)) for path_ in paths)
 
-    def filename(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+    def filename(*paths: Tuple[Path]) -> Union[str, Tuple[str]]:
         if len(paths) == 1:
-            return path.basename(paths[0]).split('.')[0]
+            return path.basename(Path(paths[0])).split('.')[0]
         else:
-            return (path.basename(path_).split('.')[0] for path_ in paths)
+            return (path.basename(Path(path_)).split('.')[0] for path_ in paths)
 
-    def extension(*paths: Tuple[str]) -> Union[str, Tuple[str]]:
+    def extension(*paths: Tuple[Path]) -> Union[str, Tuple[str]]:
         if len(paths) == 1:
-            return path.basename(paths[0]).split('.')[1]
+            return path.basename(Path(paths[0])).split('.')[1]
         else:
-            return (path.basename(path_).split('.')[1] for path_ in paths)
-
+            return (path.basename(Path(path_)).split('.')[1] for path_ in paths)
+    ext = extension
