2626775d06b7d06fe64e9f686716b28af43c6bf7
yxm <yxm.dev@gmx.ie>
2025-05-26 09:47:01 -0300

    ref: import new mods in main

--- utils/main.py
+++ utils/main.py
@@ -1,5 +1,6 @@
 from utils.mods.cmd      import cmd
 from utils.mods.json_    import json
+from utils.mods.jtable   import jtable
 from utils.mods.color    import color
 from utils.mods.text     import text
 from utils.mods.date     import date
@@ -7,3 +8,4 @@ from utils.mods.path     import path
 from utils.mods.image    import image
 from utils.mods.compress import compress
 from utils.mods.yaml_    import yaml
+from utils.mods.types    import *
