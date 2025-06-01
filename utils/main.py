bf8bf001b55c3c459eb85ef236acba3c0b4520f1
yxm <yxm.dev@gmx.ie>
2025-06-01 15:54:43 -0300

    ...

--- utils/main.py
+++ utils/main.py
@@ -2,7 +2,6 @@ from utils.mods.cmd      import cmd
 from utils.mods.json_    import json
 from utils.mods.jtable   import jtable
 from utils.mods.color    import color
-from utils.mods.text     import text
 from utils.mods.date     import date
 from utils.mods.envs     import envs
 from utils.mods.path     import path
@@ -11,4 +10,4 @@ from utils.mods.image    import image
 from utils.mods.compress import compress
 from utils.mods.yaml_    import yaml
 from utils.mods.static   import static
-from utils.mods.types    import *
+from utils.mods.str      import str
