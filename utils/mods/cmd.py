e46718ae206f9f742c367fb67a57c29387252dbe
yxm <yxm.dev@gmx.ie>
2025-10-01 19:38:31 -0300

    fix: minor fixing

--- utils/mods/cmd.py
+++ utils/mods/cmd.py
@@ -147,8 +147,9 @@ class cmd:
                 if path_to_clean.is_dir():
                     cmd.rm(path)
                     cmd.mkdir(path)
-                with open(path, 'w') as file:
-                    file.write('')
+                else:
+                    with open(path, 'w') as file:
+                        file.write('')
         except Exception as e:
             raise CmdErr(e)
 
