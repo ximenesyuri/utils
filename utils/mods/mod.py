4809cc09334228cc1d1bfc1ffc2406a4eab11f4f
yxm <yxm.dev@gmx.ie>
2025-12-27 15:09:39 -0300

    ref: improve mods.mod

--- utils/mods/mod.py
+++ utils/mods/mod.py
@@ -2,7 +2,7 @@ import importlib
 import importlib.util
 import sys
 import ast
-from typed import typed, Any, Maybe, List, Entry, Str, Bool, Dict, Union, Tuple, convert, TYPE, name, Nill
+from typed import typed, Any, Maybe, List, Entry, Str, Bool, Dict, Union, convert, TYPE, name, Nill
 from utils.err import ModErr
 from types import ModuleType
 
@@ -21,10 +21,7 @@ class mod:
             return False
 
     @typed
-    def get(obj: Maybe(Union(Str), List(Str)), module: Entry) -> Module:
-        """
-        Returns the module gect of a given module entry.
-        """
+    def get(obj: Maybe(Union(Str), List(Str)), module: Entry) -> Any:
         try:
             if mod is None:
                 if obj not in Str:
@@ -46,12 +43,16 @@ class mod:
                 return {name: getattr(module, name) for name in names}
 
             if obj in Str:
-                return getattr(module, obj)
+                return getattr(module, obj, None)
 
             if obj in List(Str):
-                return [getattr(module, name) for name in obj]
+                return [getattr(module, name, None) for name in obj]
         except Exception as e:
             raise ModErr(e)
+
+    @typed
+    def has(obj: Any, module: Entry) -> Bool:
+        return hasattr(mod.get(module), obj)
 
     @typed
     def _is_local(obj: Any) -> Bool:
@@ -68,68 +69,12 @@ class mod:
             raise ModErr(e)
 
     @typed
-    def has_local(module: Entry, *locals: Tuple) -> Bool:
-        """
-        Checks if given locals exists in a given module entry.
-        """
-        for l in locals:
-            if not mod._is_local(l):
-                raise ModErr(f"The provided object '{l}' is not a local object.")
-        return all(hasattr(mod.get(module), l) for l in locals)
-
-    @typed
-    def has_global(module: Entry, *globals: Tuple) -> Bool:
-        """
-        Checks if given globals exists in a given module entry.
-        """
-        for g in globals:
-            if not mod._is_global(g):
-                raise ModErr(f"The provided object '{g}' is not a global object.")
-        return all(hasattr(mod.get(module), g) for g in globals)
-
-    @typed
-    def get_local(module: Entry, *locals: Tuple) -> Any:
-        """
-        Get some locals from a given module entry.
-        """
-        existing_locals = []
-        for l in locals:
-            if not mod._is_local(l):
-                raise ModErr(f"The provided object '{l}' is not a local object.")
-            if mod.has_local(module, l):
-                existing_locals.append(getattr(mod.get(module), l))
-        if existing_locals:
-            if len(existing_locals) > 1:
-                return existing_locals
-            return existing_locals[0]
-        else:
-            raise ModErr(f"There are no locals '{locals}' in the given module '{module}'")
-
-    @typed
-    def get_global(module: Entry, *globals: Tuple) -> Any:
-        """
-        Get some globals from a given module entry.
-        """
-        existing_globals = []
-        for g in globals:
-            if not mod._is_global(g):
-                raise ModErr(f"The provided object '{g}' is not a global object.")
-            if mod.has_global(module, g):
-                existing_globals.append(getattr(mod.get(module), g))
-        if existing_globals:
-            if len(existing_globals) > 1:
-                return existing_globals
-            return existing_globals[0]
-        else:
-            raise ModErr(f"There are no globals '{globals}' in given module '{module}'")
-
-    @typed
     def locals(module: Entry) -> Dict:
         """
         Returns the dictionary of locals of a given module entry.
         """
         try:
-            return {name: mod.get_local(module, name) for name in dir(mod.get(module)) if name.startswith('_')}
+            return {name: mod.get(name, module) for name in dir(mod.get(module)) if mod._is_local(name)}
         except Exception as e:
             raise ModErr(e)
 
@@ -139,7 +84,7 @@ class mod:
         Returns the dictionary of globals of a given module entry.
         """
         try:
-            return {name: mod.get_global(module, name) for name in dir(mod.get(module)) if not name.startswith('_')}
+            return {name: mod.get(name, module) for name in dir(mod.get(module)) if mod._is_global(name)}
         except Exception as e:
             raise ModErr(e)
 
