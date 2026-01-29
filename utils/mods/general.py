fbf77992183c630428ee8050f03066f161a92145
yxm <yxm.dev@gmx.ie>
2026-01-29 12:23:21 -0300

    ref: turn Action into a class

--- utils/mods/general.py
+++ utils/mods/general.py
@@ -2,7 +2,7 @@ import importlib
 import sys
 import functools
 from typing import TYPE_CHECKING as __lsp__
-from typed import typed, model, Dict, Union, Maybe, Str, Bool, Bytes, Int, name, Any
+from typed import typed, model, Dict, Union, Maybe, Str, Bool, Bytes, Int, Any
 from utils.mods.json_ import Json
 from utils.mods.helper.general import Message
 from utils.mods.helper.types import Client
@@ -73,7 +73,7 @@ class result:
         )
 
     @typed
-    def data(action: Any, propagate: Bool=True, **kwargs: Dict(Str)):
+    def data(action: Any, propagate: Bool=True, **kwargs: Dict(Str)) -> Maybe(ResultData):
         res = action(**kwargs)
         if propagate:
             globals()['propagate'].failure(res)
@@ -97,32 +97,45 @@ class propagate:
         return res
 
 
-def Action(Error=None, message=None):
-    if Error is None:
-        typed_ = typed
-    if isinstance(Error, type) and issubclass(Error, BaseException):
-        if message:
-            typed_ = func.eval(typed, enclose=Error, message=message)
-        else:
-            typed_ = func.eval(typed, enclose=Error)
-    elif Error in Str:
-        if message:
-            typed_ = func.eval(typed, enclose=type(Error, (Exception,), {}), message=message)
-        else:
-            typed_ = func.eval(typed, enclose=type(Error, (Exception,), {}))
-    else:
-        raise TypeError("Error must be an exception class or a string")
+class Action:
+    success = staticmethod(result.success)
+    failure = staticmethod(result.failure)
+    data = staticmethod(result.data)
+    propagate = propagate
 
-    def decorator(func=None, **kwargs):
+    def __init__(self, Error=None, message=None):
+        if Error is None:
+            typed_ = typed
+        elif isinstance(Error, type) and issubclass(Error, BaseException):
+            if message:
+                typed_ = func.eval(typed, enclose=Error, message=message)
+            else:
+                typed_ = func.eval(typed, enclose=Error)
+        elif Error in Str:
+            exc_type = type(Error, (Exception,), {})
+            if message:
+                typed_ = func.eval(typed, enclose=exc_type, message=message)
+            else:
+                typed_ = func.eval(typed, enclose=exc_type)
+        else:
+            raise TypeError("Error must be an exception class or a string")
+
+        self._typed = typed_
+
+    def __call__(self, func=None, **kwargs):
+        typed_ = self._typed
+
         def apply(f):
             typed_func = typed_(f, **kwargs)
             cod = getattr(typed_func, "cod", None)
             if cod is not Result and not (isinstance(cod, type) and issubclass(cod, Client)):
+                from typed import name
                 raise TypeError(
                     f"Codomain mismatch in function '{name(f)}':\n"
                     "    [expected_type] 'Result'\n"
                     f"    [received_type] '{name(cod)}'"
                 )
+
             if cod is Result:
                 @functools.wraps(f)
                 def wrapper(*args, **kw):
@@ -135,15 +148,10 @@ def Action(Error=None, message=None):
                 if hasattr(typed_func, "dom"):
                     wrapper.dom = typed_func.dom
                 return wrapper
+
             return typed_func
+
         if func is None:
             return apply
         else:
             return apply(func)
-
-    decorator.success = result.success
-    decorator.failure = result.failure
-    decorator.data = result.data
-    decorator.propagate = propagate
-
-    return decorator
